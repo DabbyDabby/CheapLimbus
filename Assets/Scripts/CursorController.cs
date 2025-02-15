7828f67b29250c5a9818aa95839b2aff8c3a7642
DabbyDabby <yoondabin@gmail.com>
2025-02-15 20:27:43 +0100

    UI Line FUckup #6
    idk

--- Assets/Scripts/CursorController.cs
+++ Assets/Scripts/CursorController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+public class CursorController : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
     [SerializeField] private RectTransform dashboardArea;
     [SerializeField] private RectTransform originPoint;
@@ -14,14 +14,17 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
     private float _closestDistance;
     public Vector3 currentStartPos;
     public List<SkillSlot> selectedSkills = new List<SkillSlot>();
-    private int _currentColumnIndex = 0;
+    private int _currentColumnIndex;
+    public List<List<SkillSlot>> dashboardSlots = new List<List<SkillSlot>>();
+    private SkillSlot _currentSkillSlot;
+    public Camera cam;
     
-    private CanvasGroup _canvasGroup;
+    private CanvasGroup _cursorCanvasGroup;
     public bool isDragging;
 
     private void Awake()
     {
-        _canvasGroup = GetComponent<CanvasGroup>();
+        _cursorCanvasGroup = GetComponent<CanvasGroup>();
     }
     
     // Method for other classes to access and change the current starting position of the cursor
@@ -35,22 +38,74 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
         // Reset variables
         SetSnapped(false);
         SetStartPos(originPoint.localPosition);
+
+        dashboardSlots = new List<List<SkillSlot>>();
+        
+        _currentColumnIndex = 0;
+
+        // Populates dashboard 2D List
+        int slotPerPair = 2;
+        List<SkillSlot> pair = new List<SkillSlot>();
+        for (int i = 0; i < skillSlots.Count; i++)
+        {
+            if (i % slotPerPair == 0)
+            {
+                pair = new List<SkillSlot>();
+                dashboardSlots.Add(pair);
+            }
+            pair.Add(skillSlots[i]);
+        }
     }
 
     // Method for other classes to access and change the current snapped state of the cursor
     public void SetSnapped(bool snap) { snapped = snap; }
 
-    public void DetectSkillSelection()
+    public void HighlightSkills(int columnIndex)
     {
-        foreach (SkillSlot slot in skillSlots)
+        for (int i = 0; i < dashboardSlots.Count; i++)
         {
-            if (slot.columnIndex == 0)
+            var pair = dashboardSlots[i];
+            if (i == columnIndex)
             {
-                slot.transform.localScale = Vector3.one * 1.2f;
+                foreach(SkillSlot slot in pair)
+                {
+                    slot.SetSelectable(true);
+                    slot.transform.localScale = Vector3.one * 1.2f;
+                    Debug.Log($"SELECTABLE: {slot}");
+                } 
+            }
+            else
+            {
+                foreach(SkillSlot slot in pair)
+                {
+                    slot.SetSelectable(false);
+                    slot.transform.localScale = Vector3.one;
+                } 
             }
         }
-        
     }
+
+    public void HightlightSkillsReset()
+    {
+        for (int i = 0; i < dashboardSlots.Count; i++)
+        {
+            var pair = dashboardSlots[i];
+            
+            foreach(SkillSlot slot in pair)
+            {
+                slot.SetSelectable(false);
+                slot.transform.localScale = Vector3.one;
+            } 
+        }
+    }
+    
+
+    // Skill Selection Logic: Only 1 Skill in a column may be selected, starting from the first column, then the second, etc...
+    public void DetectSkillSelection()
+    {
+        HighlightSkills(_currentColumnIndex);
+    }
+    
     public void DetectSkillDeselection(SkillSlot skill)
     {
         Vector3 eastDirection = Vector3.right;
@@ -65,6 +120,7 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
         {
             Debug.Log("Skill deselected!");
             DeselectSkill(skill);
+            _currentColumnIndex--;
             lineDrag.RemoveSegment();
         }
     }
@@ -72,15 +128,35 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
     // Skill selection method, later used for combat start and checks for the SkillSlot script
     public void SelectSkill(SkillSlot skill)
     {
+        // Pass method if the exact same skill has already been selected
+        if (selectedSkills.Contains(skill)) return;
+        
+        // Alternation
+        // Check starts at 0 selected skills (List index = 0 means, at at least 1 selected skill)
+        for (int i = selectedSkills.Count - 1; i >= 0; i--)
+        {
+            // If last selected skill's column index is the same as the now selected one, deselect last selected skill
+            if (selectedSkills[i].columnIndex == skill.columnIndex)
+            {
+                DeselectSkill(selectedSkills[i]);
+                lineDrag.RemoveSegment();
+            }
+        }
+        
         selectedSkills.Add(skill);
+        _currentColumnIndex = skill.columnIndex;
         SetStartPos(skill.transform.localPosition);
+        
+        // Lock skill selection and check which skills can be selected next
+        DetectSkillSelection();
         Debug.Log($"Slot {skill} added");
     }
     
     // Skill removal method
     public void DeselectSkill(SkillSlot skill)
     {
-        if (selectedSkills == null) return;
+        if (selectedSkills.Count == 0) return;
+        
         selectedSkills.RemoveAt(selectedSkills.Count - 1);
         if (selectedSkills == null || selectedSkills.Count == 0)
         {
@@ -91,26 +167,19 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
             SetStartPos(selectedSkills[^1].transform.localPosition);
         }
     }
-    
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isDragging = false;
-        //lineDrag.ResetLineStart();
-    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         isDragging = true;
-        _canvasGroup.blocksRaycasts = false;
-        _canvasGroup.alpha = 0.6f;
+        _cursorCanvasGroup.blocksRaycasts = false;
+        _cursorCanvasGroup.alpha = 0.6f;
         transform.DOScale(Vector3.one * 0.5f, 0.2f);
+        
+        DetectSkillSelection();
 
     Vector2 localPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(dashboardArea, eventData.position, eventData.pressEventCamera, out localPoint);
         
-        // Scale first two skill slots for visual feedback
-        
-
         // Set the initial segment's start position at the cursor's current local position
         lineDrag.AddNewSegment(transform.localPosition);
     }
@@ -119,17 +188,62 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
     public void OnDrag(PointerEventData eventData)
     {
         if (!isDragging) return;
-
+        
         Vector2 localPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             dashboardArea, eventData.position, eventData.pressEventCamera, out localPoint
         );
-
-        DetectSkillSelection();
             
         // Smoothly move the in-game cursor to the current mouse position
         transform.localPosition = localPoint;
 
+        Vector3 mousePos = Input.mousePosition;
+        bool inRange = false;
+        
+        foreach (List<SkillSlot> pair in dashboardSlots)
+        {
+            foreach (SkillSlot slot in pair)
+            {
+                var position = RectTransformUtility.WorldToScreenPoint(null, slot.transform.position);
+                float distance = Vector3.Distance(mousePos, position);
+                Debug.Log($@"Mouse: {mousePos} || Position: {position}");
+
+                if (distance < 80 && slot != _currentSkillSlot && slot.columnIndex > _currentColumnIndex)
+                {
+                    inRange = true;
+                    _currentSkillSlot = slot;
+                    SelectSkill(slot);
+                    SetSnapped(true);
+                    lineDrag.EditCurrentSegment(slot.transform.localPosition);
+                    lineDrag.SetStartPos(slot.transform.localPosition);
+                    lineDrag.AddNewSegment(slot.transform.localPosition);
+                    break;
+                    // breaks ensure only 1 slot is selected, else it would constantly loop through each skill
+                }
+                
+            }
+            if (inRange) break;
+        }
+        
+        foreach (List<SkillSlot> pair in dashboardSlots)
+        {
+            foreach (SkillSlot slot in pair)
+            {
+                var position = slot.transform.localPosition;
+                float distance = Vector3.Distance(transform.localPosition, position);
+
+                if (distance > 81)
+                {
+                    inRange = false;
+                    SetSnapped(false);
+                    break;
+                    // breaks ensure only 1 slot is selected, else it would constantly loop through each skill
+                }
+                
+            }
+            if (!inRange) break;
+        }
+        
         if (selectedSkills.Count > 0 && !snapped)
         {
             DetectSkillDeselection(selectedSkills[^1]);
@@ -141,16 +255,17 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        HightlightSkillsReset();
         isDragging = false;
-        _canvasGroup.blocksRaycasts = true;
-        _canvasGroup.alpha = 1f;
-        transform.DOScale(Vector3.one, 0.2f);
+        _cursorCanvasGroup.blocksRaycasts = true;
+        _cursorCanvasGroup.alpha = 1f;
+        transform.DOScale(Vector3.one, 0.1f);
         // Animate snapping the cursor back to its origin position
         if (!snapped)
         {
             if (selectedSkills == null || selectedSkills.Count == 0) { 
                 Debug.Log("No skills selected, returning to TRUE origin point.");
-                currentStartPos = originPoint.localPosition;
+                SetStartPos(originPoint.localPosition);
                 lineDrag.ResetLineStart();
             }
             lineDrag.RemoveSegment();
@@ -171,7 +286,7 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
     {
         // Initialize variables
         _closestSkillSlot = null;
-        _closestDistance = 0;
+        _closestDistance = Mathf.Infinity; // Ensure the first slot checked is always closer
         
         // Get cursor position
         Vector3 cursorPosition = transform.localPosition;
@@ -184,8 +299,10 @@ public class CursorController : MonoBehaviour, IPointerDownHandler, IBeginDragHa
             // Cursor moves to Whichever skill is the closest to it 
             if (distance < _closestDistance)
             {
+                SetSnapped(true);
                 _closestDistance = distance;
                 _closestSkillSlot = socket;
+                SetStartPos(socket.transform.localPosition);
                 lineDrag.RemoveSegment();
                 transform.DOLocalMove(_closestSkillSlot.transform.localPosition, 0.0f);
             }
--- Assets/Scripts/SkillSlot.cs
+++ Assets/Scripts/SkillSlot.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 using Vector3 = UnityEngine.Vector3;
 
-public class SkillSlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+public class SkillSlot : MonoBehaviour
 {
+    //IPointerEnterHandler, 
+    //, IPointerExitHandler
+    // , IDropHandler
     public Skill skill;
     public List<Skill> skillsList;
     public int columnIndex;
@@ -16,6 +21,14 @@ public class SkillSlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoi
     public List<SkillSlot> selectedSkills;
     private bool _isSelectable = false;
     
+    private CanvasGroup _skillSlotCanvasGroup;
+    
+    private void Awake()
+    {
+        _skillSlotCanvasGroup = GetComponent<CanvasGroup>();
+        if (_skillSlotCanvasGroup == null) _skillSlotCanvasGroup = gameObject.AddComponent<CanvasGroup>();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,6 +37,7 @@ public class SkillSlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoi
         {
             Destroy(slot.gameObject);
         }
+        
         selectedSkills.Clear();
     }
     
@@ -36,53 +50,61 @@ public class SkillSlot : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoi
     public void SetSelectable(bool selectable)
     {
         _isSelectable = selectable;
+        _skillSlotCanvasGroup.interactable = selectable;
+        _skillSlotCanvasGroup.blocksRaycasts = selectable;
     }
     
     // When the cursor is dropped onto the skill slot, snap cursor to skill slot
-    public void OnDrop(PointerEventData eventData)
-    {
-        Debug.Log("OnDrop triggered.");
-        if (eventData.pointerDrag != null && cursor.isDragging)
-        {
-            // Snap the dragged cursor to the slot's local position
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-            cursor.SetSnapped(true);
-        }
-    }
+    // public void OnDrop(PointerEventData eventData)
+    // {
+    //     Debug.Log("OnDrop triggered.");
+    //     if (eventData.pointerDrag != null && cursor.isDragging)
+    //     {
+    //         // Snap the dragged cursor to the slot's local position
+    //         eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+    //         cursor.SetSnapped(true);
+    //     }
+    // }
     
     // When the cursor enters a skill slot's radius
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        // If the cursor is being dragged and there is no segment attached to the skill slot
-        // Tell the cursor it's being snapped to the closest skill and "select" it
-        // Set the cursor's starting position to the skill slot's position
-        // Once cursor enters the skill's radius, the last line segment's end point snaps onto the skill slot
-        // Set the next segment's starting point to the skill slot's position and create a new segment
-        // "hasSegment" to prevent prefab spam of line segments
-        // Scale the current skill slot for visual feedback
-        if (cursor.isDragging && !_hasSegment) {
-            cursor.SelectSkill(this);
-            cursor.SetSnapped(true);
-            lineDrag.EditCurrentSegment(transform.localPosition);
-            lineDrag.SetStartPos(transform.localPosition);
-            lineDrag.AddNewSegment(transform.localPosition);
-            _hasSegment = true;
-            transform.localScale = Vector3.one * 1.2f;
-            Debug.Log($"✅ Cursor SNAPPED");
-        }
-    }
+    // public void OnPointerEnter(PointerEventData eventData)
+    // {
+    //     // If the cursor is being dragged and there is no segment attached to the skill slot
+    //     // Tell the cursor it's being snapped to the closest skill and "select" it
+    //     // Set the cursor's starting position to the skill slot's position
+    //     // Once cursor enters the skill's radius, the last line segment's end point snaps onto the skill slot
+    //     // Set the next segment's starting point to the skill slot's position and create a new segment
+    //     // "hasSegment" to prevent prefab spam of line segments
+    //     // Scale the current skill slot for visual feedback
+    //     if (cursor.isDragging && !_hasSegment) {
+    //         
+    //         cursor.SetSnapped(true);
+    //         lineDrag.EditCurrentSegment(transform.localPosition);
+    //         lineDrag.SetStartPos(transform.localPosition);
+    //         lineDrag.AddNewSegment(transform.localPosition);
+    //         _hasSegment = true;
+    //         //transform.localScale = Vector3.one * 1.2f;
+    //         Debug.Log($"✅ Cursor SNAPPED"); 
+    //     }
+    // }
 
     // When the cursor exits a skill slot's radius
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        // If the cursor is being dragged, tell the cursor it is no longer snapped to that skill slot
-        // Shrink skill slot back for visual feedback
-        if (cursor.isDragging)
-        {
-            cursor.SetSnapped(false);
-            transform.localScale = Vector3.one;
-            Debug.Log($"XXX Cursor no longer snapped.");
-            _hasSegment = false;
-        }
-    }
+    // public void OnPointerExit(PointerEventData eventData)
+    // {
+    //     // If the cursor is being dragged, tell the cursor it is no longer snapped to that skill slot
+    //     // Shrink skill slot back for visual feedback
+    //     Vector3 eastDirection = Vector3.right;
+    //     
+    //     Vector3 directionToCursor = (cursor.transform.localPosition - transform.localPosition).normalized;
+    //     
+    //     float distance = Vector3.Distance(eventData.position, transform.localPosition); 
+    //     
+    //     if (cursor.isDragging && distance > 100 && _hasSegment)
+    //     {
+    //         cursor.SetSnapped(false);
+    //         //transform.localScale = Vector3.one;
+    //         Debug.Log($"XXX Cursor no longer snapped.");
+    //         _hasSegment = false;
+    //     }
+    // }
 }
